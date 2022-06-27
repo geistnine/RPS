c5b3c54890b6f838b2d93e849faecb4b971a5717
Steve <sgeiger5516@gmail.com>
2022-06-27 17:22:24 -0400

    Created working game function. RPS 1.0

--- rpsscript.js
+++ rpsscript.js
@@ -1,7 +1,7 @@
 let choices = ["rock", "paper", "scissors"]
 
 function computerPlay(){
-    let cpuSelection = Math.floor(Math.random() * 3 + 1)
+    let cpuSelection = Math.floor(Math.random() * 3)
     return cpuSelection
 }
 
@@ -12,13 +12,22 @@ function mod(a, b) {
 
 function RPS(choice1, choice2){
     if (choice1 === choice2){
-        return "It's a tie!"
+        return "It's a draw!"
     } else if (mod(choice1 - choice2, choices.length) < choices.length / 2) {
-        return "You win!"
+        return "You win! " + choices[choice1] + " beats " + choices[choice2] + "!"
     } else {
-        return "You lose!"
+        return "You lose! " + choices[choice2] + " beats " + choices[choice1] + "!"
     }
 }
 
+function game(){
+    for (let i = 0; i < 5; i++){
+        let selection = prompt("Rock, paper, or scissors?").toLowerCase()
+        let playerChoice = choices.indexOf(selection)
+        let cpuChoice = computerPlay()
+        alert(RPS(playerChoice, cpuChoice))
+    }
+    return "Thanks for playing!"
+}
 
-console.log(computerPlay())
+console.log(game())
