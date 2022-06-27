52d6bd3c522457169093b921472757dc909b4502
Steve <sgeiger5516@gmail.com>
2022-06-27 16:29:04 -0400

    Added RPS function

--- rpsscript.js
+++ rpsscript.js
@@ -10,5 +10,15 @@ function mod(a, b) {
     return c < 0 ? c + b : c;
 }
 
+function RPS(choice1, choice2){
+    if (choice1 === choice2){
+        return "It's a tie!"
+    } else if (mod(choice1 - choice2, choices.length) < choices.length / 2) {
+        return "You win!"
+    } else {
+        return "You lose!"
+    }
+}
+
 
 console.log(computerPlay())
